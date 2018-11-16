abeeb9e5a0f4807049f53025e6b94b1cf7740c5a
cdgira <cdgira@cs.ship.edu>
2018-11-16 16:39:53 -0500

    Got two more beat paths added.

--- core/src/com/game/obj/Beat.java
+++ core/src/com/game/obj/Beat.java
@@ -1,6 +1,7 @@
 package com.game.obj;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
 import com.game.helper.AudioManager;
 
 import box2dLight.Light;
@@ -17,7 +18,16 @@ public class Beat
 {
     Color[] colors = {Color.PURPLE, Color.YELLOW, Color.RED, Color.BLUE, Color.GREEN, 
 	    Color.BROWN, Color.FIREBRICK, Color.GOLDENROD, Color.MAROON, Color.FOREST};
+    // TODO: Not sure if we need this given all movement plotted relative to endPoint
+    Vector2[] startPoints = {new Vector2(240,460), new Vector2(1010, 460), new Vector2(625,175), new Vector2(300,460),
+	    new Vector2(300,460), new Vector2(300,460), new Vector2(300,460), new Vector2(300,460),
+	    new Vector2(300,460), new Vector2(300,460)
+    };
     
+    Vector2[] endPoints = {new Vector2(440,460), new Vector2(810,460), new Vector2(625,275), new Vector2(440,460), 
+	    new Vector2(440,460), new Vector2(440,460), new Vector2(440,460), new Vector2(440,460),
+	    new Vector2(440,460), new Vector2(440,460) 
+    };
     
     private Light approachLight;
     private Light beatLight;
@@ -54,8 +64,10 @@ public class Beat
 	end = e;
         type = t;
         // TODO: Needs a set start point for each type and end point.
-	approachLight = new PointLight(rayHandler, 100, Color.WHITE, 0f, 300f, 460f);
-	beatLight = new PointLight(rayHandler, 100, colors[type], 0f, 440f, 460f);
+        Vector2 startPoint = startPoints[type];
+        Vector2 endPoint = endPoints[type];
+	approachLight = new PointLight(rayHandler, 100, Color.WHITE, 0f, startPoint.x, startPoint.y);
+	beatLight = new PointLight(rayHandler, 100, colors[type], 0f, endPoint.x, endPoint.y);
     }
     
     public void update(float deltaTime)
@@ -78,7 +90,17 @@ public class Beat
 	{
 
 	    approachLight.setDistance(50f);
-	    approachLight.setPosition(440-scale*200, approachLight.getY());
+	    if (endPoints[type].x == 625)
+	    {
+		approachLight.setPosition(startPoints[type].x, endPoints[type].y-scale*100);
+	    }
+	    else
+	    {
+	        if (endPoints[type].x > 700)
+		    approachLight.setPosition(endPoints[type].x+scale*200, approachLight.getY());
+	        else
+	            approachLight.setPosition(endPoints[type].x-scale*200, approachLight.getY());
+	    }
 	}
 	else if (((int)beatLight.getDistance() == 0) && (!fired))
 	{    
